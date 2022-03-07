daf858dbb81a9ada56a9a7e5a2f2dbd1d60de1a9
Dpr1815 <91048238+Dpr1815@users.noreply.github.com>
2022-03-07 16:51:14 +0100

    Create mint1155.sol

---
+++ mint1155.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract NFTContract is ERC1155, Ownable {
+
+
+    constructor(
+         
+         string memory uri,
+         uint256[] memory ids,
+         uint256[] memory amount,
+         address owner
+    )
+        ERC1155( uri
+            // "https://ipfs.moralis.io:2053/ipfs/QmS7izjgMprD3ZvP8aDBRiXbMZnxBsrUK4PJTwkcDFauij" 
+            )
+    {
+      
+  _mintBatch(owner, ids,amount,"");
+    }
+     
+
+
+}
